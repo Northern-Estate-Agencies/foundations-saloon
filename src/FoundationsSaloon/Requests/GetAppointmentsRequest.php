974d9c4da257e9c97e14cc3b3f5327e45508394d
Rory Lockett <rory.lockett@northernestateagencies.co.uk>
2024-09-19 17:20:43 +0100

    making appointments paginateable

--- src/FoundationsSaloon/Requests/GetAppointmentsRequest.php
+++ src/FoundationsSaloon/Requests/GetAppointmentsRequest.php
@@ -4,8 +4,9 @@ namespace FoundationsSaloon\Requests;
 
 use Saloon\Enums\Method;
 use Saloon\Http\Request;
+use Saloon\PaginationPlugin\Contracts\Paginatable;
 
-class GetAppointmentsRequest extends Request
+class GetAppointmentsRequest extends Request implements Paginatable
 {
     protected Method $method = Method::GET;
 
