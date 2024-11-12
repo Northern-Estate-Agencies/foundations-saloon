4536e64338cedc5bb1fd5f0cc596f8304a9dd5b2
Matthew Gadd <matthew.gadd@asafe.com>
2024-11-12 17:26:09 +0000

    WDT-1138-add-a-companies-request-to-foundations-saloon

---
+++ src/FoundationsSaloon/Requests/GetCompanyRequest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace FoundationsSaloon\Requests;
+
+use Saloon\Enums\Method;
+use Saloon\Http\Request;
+
+class GetPropertyRequest extends Request
+{
+    protected Method $method = Method::GET;
+
+    public function resolveEndpoint(): string
+    {
+        return '/companies/' . $this->id;
+    }
+
+    public function __construct(
+        protected string $id,
+    ) {}
+}
