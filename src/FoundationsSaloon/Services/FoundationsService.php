efefd5dd308ebbe8e6ad96c8560761e1490adec2
Matthew Gadd <matthew.gadd@northernestateagencies.co.uk>
2026-07-21 09:11:02 +0100

    setting up authentication

--- src/FoundationsSaloon/Services/FoundationsService.php
+++ src/FoundationsSaloon/Services/FoundationsService.php
@@ -1,5 +1,34 @@
 <?php
 
-class FoundationsService {
-    
+use FoundationsSaloon\FoundationsConnector;
+use Saloon\Http\Auth\AccessTokenAuthenticator;
+
+class FoundationsService
+{
+    protected FoundationsConnector $connector;
+
+    protected AccessTokenAuthenticator $authenticator;
+
+    public function __construct(
+        ?FoundationsConnector $connector = null,
+    ) {
+        $this->setUpAuthentication($connector);
+    }
+
+    private function setUpAuthentication(?FoundationsConnector $connector = null): void
+    {
+        if ($connector === null) {
+            $connector = new FoundationsConnector();
+        }
+
+        $this->connector = $connector;
+        $this->authenticator = new AccessTokenAuthenticator('');
+
+        if (config('app.env', 'testing') !== 'testing') {
+            /** @var AccessTokenAuthenticator $authenticator */
+            $authenticator = $this->connector->getAccessToken();
+            $this->authenticator = $authenticator;
+            $this->connector->authenticate($this->authenticator);
+        }
+    }
 }
